62e95fda82adb88d0d40a3a71c5608bd42a34cf3
Andy Lau <anl225@ipod.lehigh.edu>
2023-11-09 16:14:08 -0500

    renamed shell script for finding files, included changing extensions functionality

--- find_mp3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the root folder name
-root_folder=drive_01_LU_29400 # CHANGE this
-
-# Find MP3 files and save the list to a text file with the root folder name
-find ${root_folder} -type f -name "*.mp3" > "${root_folder}_mp3_files_list.txt"
